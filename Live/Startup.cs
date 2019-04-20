86896700754a2882bcd914e653ab3d896c266e69
jriley15 <jordanr3@live.com>
2019-04-19 22:18:45 -0700

    pls cors

--- Live/Startup.cs
+++ Live/Startup.cs
@@ -134,7 +134,7 @@ namespace Live
                 //policy.AllowAnyOrigin();
                 //policy.WithOrigins("http://localhost:3000", "http://192.168.1.17", "https://live.jrdn.tech", "http://98.171.80.97:3000");
                 //policy.SetIsOriginAllowed((host) => true);
-                policy.WithOrigins("http://192.168.1.14", "https://192.168.1.14", "http://localhost");
+                policy.WithOrigins("http://192.168.1.14", "https://192.168.1.14", "http://localhost", "https://192.168.1.10", "http://192.168.1.10");
                 policy.AllowCredentials();
             });
 
